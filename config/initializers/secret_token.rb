cda8ddd085f25dc8950fa9885381b06facf0e186
Gautham Narayanasamy <gautham.n.sam@gmail.com>
2012-10-27 13:16:39 -0700

    Created the rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinjaChallenge::Application.config.secret_token = '70df0aef2af5476516e0f6e0cba605bbbc98b3b45f2d066ca8066e8976d00676dbad3f68c253af120007b89d7de6e09d7501366f2cd79e9a285a0e1f2ff49020'
