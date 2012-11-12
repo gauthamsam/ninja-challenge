1024828a7ac1d5eb00b4e05be5ec352b937220d8
prakash chandrasekaran <techianprakash@gmail.com>
2012-11-11 16:46:13 -0800

    null checks added to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,14 @@
 class Test < ActiveRecord::Base
   attr_accessible :admin_id, :end_date, :level_id, :max_score, :name, :start_date
   
+  
+  #validates :end_date,  :presence => true
+  #validates :start_date, :presence => true
+  #validates :level_id, :presence => true
+  #validates :max_score, :presence => true
+  #validates :name, :presence => true
+  
+ 
   has_one :admin
   has_many :test_questions
 end
